e3b1c89c4a06de39c3cf0b98903820a9e46f2537
Rico Sonntag <rico.sonntag@netresearch.de>
2022-11-25 15:40:34 +0000

    FX-507: Add people and person response model

---
+++ src/Model/People/Person.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * This file is part of the package netresearch/sdk-api-central-station.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\Sdk\CentralStation\Model\People;
+
+use DateTime;
+
+/**
+ * A person record.
+ *
+ * @author  Rico Sonntag <rico.sonntag@netresearch.de>
+ * @license Netresearch https://www.netresearch.de
+ * @link    https://www.netresearch.de
+ */
+class Person
+{
+    /**
+     * ID of a person.
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * ID of account.
+     *
+     * @var int
+     */
+    public $accountId;
+
+    /**
+     * The salutation of the person.
+     *
+     * @var null|string
+     */
+    public $salutation;
+
+    /**
+     * The title of the person.
+     *
+     * @var null|string
+     */
+    public $title;
+
+    /**
+     * The title of the person.
+     *
+     * @var null|string
+     */
+    public $gender;
+
+    /**
+     * The language of the person as ISO-639-1 code (e.g. de, en, fr or NULL).
+     *
+     * @var null|string
+     */
+    public $countryCode;
+
+    /**
+     * The first name of the person.
+     *
+     * @var null|string
+     */
+    public $firstName;
+
+    /**
+     * The last name of the person.
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * Background information about the person.
+     *
+     * @var null|string
+     */
+    public $background;
+
+    /**
+     * ID of the user if the person belongs to a user.
+     *
+     * @var null|int
+     */
+    public $userId;
+
+    /**
+     * Time of creation.
+     *
+     * @var null|DateTime
+     */
+    public $createdAt;
+
+    /**
+     * Time of last update.
+     *
+     * @var null|DateTime
+     */
+    public $updatedAt;
+}
